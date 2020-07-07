ce9b195ea7543c26fbba7c8d226e34f69a86e223
radek_random <jozwa.zawadiaka@gmail.com>
2020-07-08 00:17:18 +0200

    get

--- app.js
+++ app.js
@@ -38,7 +38,17 @@ var authLimiter = rateLimit({
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
-
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin,X-Requested-With,Content-Type,Accept,content-type,application/json"
+  );
+  res.header("Authorization", "*");
+  next();
+});
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
